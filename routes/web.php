1dfc18cf2687e0527458220cf052a7dd38d637b8
KuyaRey <r.buscagan.550108@umindanao.edu.ph>
2025-04-24 20:51:12 +0800

    Complete Task Management System with screenshots

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
